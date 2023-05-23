09bb16736d5259924f9f1b6850ed9e390d278a14
Erin <eplewis89@users.noreply.github.com>
2023-05-23 15:16:53 -0700

    commit current work

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,14 +5,30 @@ import path from "path"
 export const register = (app: express.Application) => {
     // home page
     app.get("/", (req: any, res) => {
-        var file = path.resolve(__dirname, "../public/assets/text/original.txt")
+        var targetChars: string[][] = [];
 
-        fs.readFile(file, "utf8", (error,data) => {
-            if (error) {
-                res.render("index", { 'text': 'file not found...' });
-            } else {
-                res.render("index", { 'text': data.split('\n') });
-            }
+        var rowColData = {
+            'minRow': 2,
+            'maxRow': 5,
+            'minCol': 9,
+            'maxCol': 50
+        };
+
+        var originalFile = path.resolve(__dirname, "../public/assets/text/original.txt")
+        var targetFile = path.resolve(__dirname, "../public/assets/text/final.txt")
+
+        fs.readFile(targetFile, "utf8", (error, target) => {
+            target.split('\n').forEach(element => {
+                targetChars.push(element.split(''));
+            });
+
+            fs.readFile(originalFile, "utf8", (error,data) => {
+                if (error) {
+                    res.render("index", { 'text': 'file not found...' });
+                } else {
+                    res.render("index", { 'text': data.split('\n'), 'target': targetChars, 'rules': rowColData });
+                }
+            });
         });
     });
 
