17450660cbbbfe39abc0ad32e17610671dbced8e
Erin <eplewis89@users.noreply.github.com>
2023-05-23 15:20:38 -0700

    building again

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -9,14 +9,27 @@ const path_1 = __importDefault(require("path"));
 const register = (app) => {
     // home page
     app.get("/", (req, res) => {
-        var file = path_1.default.resolve(__dirname, "../public/assets/text/original.txt");
-        fs_1.default.readFile(file, "utf8", (error, data) => {
-            if (error) {
-                res.render("index", { 'text': 'file not found...' });
-            }
-            else {
-                res.render("index", { 'text': data.split('\n') });
-            }
+        var targetChars = [];
+        var rowColData = {
+            'minRow': 2,
+            'maxRow': 5,
+            'minCol': 9,
+            'maxCol': 50
+        };
+        var originalFile = path_1.default.resolve(__dirname, "../public/assets/text/original.txt");
+        var targetFile = path_1.default.resolve(__dirname, "../public/assets/text/final.txt");
+        fs_1.default.readFile(targetFile, "utf8", (error, target) => {
+            target.split('\n').forEach(element => {
+                targetChars.push(element.split(''));
+            });
+            fs_1.default.readFile(originalFile, "utf8", (error, data) => {
+                if (error) {
+                    res.render("index", { 'text': 'file not found...' });
+                }
+                else {
+                    res.render("index", { 'text': data.split('\n'), 'target': targetChars, 'rules': rowColData });
+                }
+            });
         });
     });
     // about page
