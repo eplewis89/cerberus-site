4841f3cdba7ceb7fa566d43fe3a650c15fcf24c3
Erin Patrick Lewis <eplewis89@gmail.com>
2023-07-27 13:58:38 -0700

    fix issue

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,9 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.register = void 0;
+const mysql_1 = __importDefault(require("mysql"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const register = (app, db) => {
+const register = (app) => {
+    // setup database
+    const db = mysql_1.default.createConnection(process.env.DATABASE_URL);
+    db.connect();
+    var examplePost = {
+        date_posted: new Date(),
+        id: 0,
+        title: "Example Title",
+        subtitle: "Example Subtitle",
+        content: "Example Content"
+    };
     // home page
     app.get("/", (req, res) => {
         var targetChars = [];
@@ -26,33 +37,35 @@ const register = (app, db) => {
             });
         });
     });
-    // about page
-    app.get("/about", (req, res) => {
-        var selfieFile = path_1.default.resolve(__dirname, "../public/assets/text/selfie.txt");
-        fs_1.default.readFile(selfieFile, "utf8", (error, data) => {
-            if (error) {
-                res.render("about", { 'selfie': 'file not found...' });
-            }
-            else {
-                res.render("about", { 'selfie': data.split('\n') });
-            }
-        });
-    });
     // blog posts
     app.get("/thoughts", (req, res) => {
-        db.query('SELECT * FROM thoughts WHERE id = 1', (err, rows, fields) => {
-            if (err)
-                res.render("error", { 'error': "application error occurred" });
-            res.render("thoughts", { 'isBlog': true, 'posts': rows });
-        });
+        if (db != null) {
+            db.query('SELECT * FROM thoughts WHERE id = 1', (err, rows, fields) => {
+                if (err)
+                    res.render("error", { 'error': "application error occurred" });
+                res.render("thoughts", { 'isBlog': true, 'posts': rows });
+            });
+        }
+        else {
+            res.render("thoughts", { 'isBlog': true, 'posts': [examplePost] });
+        }
     });
     // specific post
     app.get("/thoughts/:id", (req, res) => {
-        db.query(`SELECT * FROM thoughts WHERE id = ${req.params.id}`, (err, rows, fields) => {
-            if (err)
-                res.render("error", { 'error': "post not found" });
-            res.render("thoughts", { 'isBlog': false, 'post': rows[0] });
-        });
+        if (db != null) {
+            db.query(`SELECT * FROM thoughts WHERE id = ${req.params.id}`, (err, rows, fields) => {
+                if (err)
+                    res.render("error", { 'error': "post not found" });
+                res.render("thoughts", { 'isBlog': false, 'post': rows[0] });
+            });
+        }
+        else {
+            res.render("thoughts", { 'isBlog': false, 'post': examplePost });
+        }
+    });
+    // about page
+    app.get("/about", (req, res) => {
+        res.render("about");
     });
 };
 exports.register = register;
