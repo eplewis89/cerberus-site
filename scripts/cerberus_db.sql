30e53f4c53c7f422568621cecd9803cf0ac99d90
Erin <eplewis89@gmail.com>
2024-02-21 12:17:37 -0800

    fix

--- scripts/cerberus_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE thoughts(
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    date_posted DATETIME NOT NULL
-) AUTO_INCREMENT=1000;
-
-INSERT INTO thoughts (id, title, content, date_posted) VALUES
-  (1, 'congratulations!', 'you have somehow found this post',  NOW()),
